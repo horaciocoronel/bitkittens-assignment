c8d0e7120c128d245c545cbef8e9922c4248abbf
horaciocoronel <arielc@gmail.com>
2017-11-15 15:19:41 -0500

    Add Event Listener to Button, create Ajax call to API

--- cats.js
+++ cats.js
@@ -1,4 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
   var summonCatsBtn = document.querySelector('.summon-cats');
+  var url = 'http://bitkittens.herokuapp.com/cats.json';
 
-});
+  summonCatsBtn.addEventListener('click', function(){
+    $.ajax({
+      url: url,
+      method: 'GET',
+      dataType: 'json'
+    })
+  })
+
+
+
+}); //ending of DOMContentLoaded
